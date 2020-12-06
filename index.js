658eebc5f05ec66a6fabcd5d5df87238aadb6888
NPC-7 <75063258+NPC-7@users.noreply.github.com>
2020-12-06 15:59:03 +0700

    adding supervillains node module and try the module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //jshint ES-version:6
 
 var superheroes = require("superheroes");
+var supervillains = require("supervillains");
 
 var mySuperheroes = superheroes.random();
+var mySupervillains = supervillains.random();
 
-console.log(mySuperheroes);
+console.log("====HEROES VS VILLAINS====");
+console.log("Hero: "+mySuperheroes);
+console.log("Villain: "+mySupervillains);
